2c04f357394d7057e1c15c5611c79b05364b2c52
sandner.art | <134306627+sandner-art@users.noreply.github.com>
2024-12-07 13:04:35 +0100

    changing imports

--- engine/game_engine.py
+++ engine/game_engine.py
@@ -1,6 +1,8 @@
 import json
 import os
 import random
+from engine import media_player
+from engine.battle_system import BattleSystem
 
 class GameEngine:
     def __init__(self, scenes, items, characters, story_texts_filename):
